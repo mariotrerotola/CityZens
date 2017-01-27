708a6542e496069c8adbf0cad9abe0912dd2e56e
Vitali Karabchuk <vitaly.korobchuk@gmail.com>
2017-01-27 11:40:07 +0100

    initial commit

---
+++ App mini challenge/ObtainLocation.swift
@@ -0,0 +1,12 @@
+//
+//  ObtainLocation.swift
+//  App mini challenge
+//
+//  Created by Mario Trerotola on 27/01/2017.
+//  Copyright © 2017 Mario Trerotola. All rights reserved.
+//
+
+import Foundation
+import MapKit
+var managerPosizione: CLLocationManager!
+var posizioneUtente: CLLocationCoordinate2D!
